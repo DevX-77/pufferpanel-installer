d49c5174be1d116fa5c501c96a4c647a706b0aa2
Devbhai7 <kumawatnirmala92@gmail.com>
2024-06-17 18:22:11 +0530

    Update pufferpanel.sh

--- pufferpanel.sh
+++ pufferpanel.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 clear
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-RED='\033[0;31m'
 echo "
 #######################################################################################
 #
@@ -21,80 +18,75 @@ read option
 
 if [ $option -eq 1 ]; then
     clear
-    echo -e "${RED}Downloading... Please Wait"
-     apt update && apt upgrade -y
-     apt install git curl wget sudo lsof iputils-ping -y
-     curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py
-     chmod -R 777 /bin/systemctl
-     clear
-    echo -e "${GREEN}Basic Packages Installed!" 
-    echo -e "${RED}sudo / curl / wget / git / lsof / ping"
+    echo "Downloading... Please Wait"
+    apt update && apt upgrade -y
+    apt install git curl wget sudo lsof iputils-ping -y
+    apt install systemctl
+    curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py
+    chmod +x /bin/systemctl
+    clear
+    echo "Basic Packages Installed!" 
+    echo "sudo / curl / wget / git / lsof / ping"
 elif [ $option -eq 2 ]; then
     clear
-    echo -e "${RED}Downloading... Please Wait"
+    echo "Downloading... Please Wait"
     apt update && apt upgrade -y
-    export SUDO_FORCE_REMOVE=yes
-    apt remove sudo -y
     apt install curl wget git python3 -y
     curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh | bash
-    apt update && apt upgrade -y
-    apt install pufferpanel
+    apt update && apt install pufferpanel -y
     clear
-    echo -e "${GREEN}PufferPanel installation completed!"
-    echo -e "${YELLOW}Enter PufferPanel Port"
+    echo "PufferPanel installation completed!"
+    echo "Enter PufferPanel Port"
     read pufferPanelPort
 
     sed -i "s/\"host\": \"0.0.0.0:8080\"/\"host\": \"0.0.0.0:$pufferPanelPort\"/g" /etc/pufferpanel/config.json
-    echo -e "${YELLOW}Enter the username for the admin user:"
+    echo "Enter the username for the admin user:"
     read adminUsername
-    echo -e "${YELLOW}Enter the password for the admin user:"
+    echo "Enter the password for the admin user:"
     read adminPassword
-    echo -e "${YELLOW}Enter the email for the admin user:"
+    echo "Enter the email for the admin user:"
     read adminEmail
 
     pufferpanel user add --name "$adminUsername" --password "$adminPassword" --email "$adminEmail" --admin
     clear
-    echo -e "${GREEN}Admin user $adminUsername added successfully!${NC}"
+    echo "Admin user $adminUsername added successfully!"
     systemctl restart pufferpanel
     clear
-    echo -e "${GREEN}PufferPanel Created & Started - PORT: ${NC}$pufferPanelPort${GREEN}"
+    echo "PufferPanel Created & Started - PORT: $pufferPanelPort"
 elif [ $option -eq 3 ]; then
     clear
-    echo -e "${RED}Downloading... Please Wait"
+    echo "Downloading... Please Wait"
     apt update && apt upgrade -y
-    export SUDO_FORCE_REMOVE=yes
-    apt remove sudo -y
     apt install curl wget git python3 -y
     curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh | bash
-    apt update && apt upgrade -y
-    apt install pufferpanel
+    apt update && apt install pufferpanel -y
     clear
-    echo -e "${GREEN}PufferPanel installation completed!"
-    echo -e "${YELLOW}Enter PufferPanel Port"
+    echo "PufferPanel installation completed!"
+    echo "Enter PufferPanel Port"
     read pufferPanelPort
 
     sed -i "s/\"host\": \"0.0.0.0:8080\"/\"host\": \"0.0.0.0:$pufferPanelPort\"/g" /etc/pufferpanel/config.json
-    echo -e "${YELLOW}Enter the username for the admin user:"
+    echo "Enter the username for the admin user:"
     read adminUsername
-    echo -e "${YELLOW}Enter the password for the admin user:"
+    echo "Enter the password for the admin user:"
     read adminPassword
-    echo -e "${YELLOW}Enter the email for the admin user:"
+    echo "Enter the email for the admin user:"
     read adminEmail
 
     pufferpanel user add --name "$adminUsername" --password "$adminPassword" --email "$adminEmail" --admin
     clear
-    echo -e "${GREEN}Admin user $adminUsername added successfully!${NC}"
+    echo "Admin user $adminUsername added successfully!"
     systemctl restart pufferpanel
     clear
-    echo -e "${GREEN}PufferPanel Created & Started - PORT: ${NC}$pufferPanelPort${GREEN}"
-    echo -e "${RED}Installing Ngrok... Please Wait"
+    echo "PufferPanel Created & Started - PORT: $pufferPanelPort"
+    echo "Now Installing Ngrok... Please Wait"
     wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
     tar -xf ngrok-v3-stable-linux-amd64.tgz
-    echo -e "${YELLOW}Enter Your Ngrok Auth Token:${NC}"
+    echo "Enter Your Ngrok Auth Token (get it from ngrok.com)"
     read NgrokAuthToken
     ./ngrok config add-authtoken $NgrokAuthToken
-    echo -e "${YELLOW}Enter the port you want to tunnel:${NC}"
+    echo "Enter the port you want to tunnel:"
     read port
-    echo -e "${GREEN}Starting Ngrok tunnel on port $port...${NC}"
+    echo "Starting Ngrok tunnel on port $port..."
     ./ngrok http $port &
-fi    
+fi
