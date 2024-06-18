de4ec6d311a2d081c19ffb070ca847f43def325d
Devbhai7 <kumawatnirmala92@gmail.com>
2024-06-18 11:09:36 +0530

    Update pufferpanel.sh

--- pufferpanel.sh
+++ pufferpanel.sh
@@ -102,4 +102,5 @@ elif [ $option -eq 3 ]; then
         clear
         ngrok_url=$(curl -s http://127.0.0.1:4040/api/tunnels | jq -r '.tunnels[0].public_url')
         echo "Ngrok started successfully! Access your tunnel at: $ngrok_url"
+        fi
 fi
