f0b257f03f94f745f1482d0caa0926e7904c20ba
Devbhai7 <kumawatnirmala92@gmail.com>
2024-06-25 08:55:42 +0530

    Update pufferpanel.sh

--- pufferpanel.sh
+++ pufferpanel.sh
@@ -10,7 +10,7 @@ echo "
 #
 #######################################################################################"
 echo "Select an option:"
-echo "1) Install Basic Packages (optional)"
+echo "1) Install Basic Packages (not recommend)"
 echo "2) PufferPanel"
 echo "3) PufferPanel & Ngrok"
 echo "4) COMING SOON"
@@ -25,8 +25,9 @@ fi
 if [ "$option" -eq 1 ]; then
     clear
     echo "Downloading... Please Wait"
-    apt update && apt upgrade -y
-    apt install git curl wget sudo lsof iputils-ping -y
+    apt update && apt upgrade
+    apt install git curl wget sudo lsof iputils-ping
+    apt install systemctl
     curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py
     chmod -R 777 /bin/systemctl
     clear
@@ -35,16 +36,11 @@ if [ "$option" -eq 1 ]; then
 elif [ "$option" -eq 2 ]; then
     clear
     echo "Downloading... Please Wait"
-    apt update && apt upgrade -y
-    export SUDO_FORCE_REMOVE=yes
-    apt remove sudo -y
-    apt install curl wget git python3 -y
     curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh | bash
-    apt update && apt upgrade -y
     apt install pufferpanel
     clear
     echo "PufferPanel installation completed!"
-    echo "Enter PufferPanel Port"
+    echo "Enter PufferPanel Port (8080)"
     read pufferPanelPort
 
     sed -i "s/\"host\": \"0.0.0.0:8080\"/\"host\": \"0.0.0.0:$pufferPanelPort\"/g" /etc/pufferpanel/config.json
@@ -64,16 +60,11 @@ elif [ "$option" -eq 2 ]; then
 elif [ "$option" -eq 3 ]; then
     clear
     echo "Downloading... Please Wait"
-    apt update && apt upgrade -y
-    export SUDO_FORCE_REMOVE=yes
-    apt remove sudo -y
-    apt install curl wget git python3 -y
     curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh | bash
-    apt update && apt upgrade -y
     apt install pufferpanel
     clear
     echo "PufferPanel installation completed!"
-    echo "Enter PufferPanel Port"
+    echo "Enter PufferPanel Port (8080)"
     read pufferPanelPort
 
     sed -i "s/\"host\": \"0.0.0.0:8080\"/\"host\": \"0.0.0.0:$pufferPanelPort\"/g" /etc/pufferpanel/config.json
@@ -102,7 +93,7 @@ elif [ "$option" -eq 3 ]; then
     read install_choice
 
     if [ "$install_choice" == "yes" ]; then
-        echo "Please setup Ngrok tunnel manually and run this script again to set up the tunnel automatically."
+        echo "Please setup Ngrok tunnel manually."
         exit 0
     else
         echo "Enter the port you want to tunnel:"
