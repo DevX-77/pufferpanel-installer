c155e986eca749160cbbd98438c61b7ebd8592b5
DevX <kumawatnirmala92@gmail.com>
2024-09-01 12:03:55 +0530

    Update pufferpanel.sh

--- pufferpanel.sh
+++ pufferpanel.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 clear
 echo 
-"#########################################################################
-                                                                                    
-   Project 'pufferpanel-installer'                                                    
-                                                                                    
-   Copyright (C) 2024, DevX                                                          
-                                                                                    
+"#######################################################################                                                                                  
+   Project 'pufferpanel-installer'                                                                                                                                      
+   Copyright (C) 2024, DevX                                                                                                                                         
    This program is free software: you can redistribute it and/or modify            
-   it under the terms of the MIT License.                                           
-                                                                                    
+   it under the terms of the MIT License.                                                                                                                        
    This program is distributed in the hope that it will be useful,                  
    but WITHOUT ANY WARRANTY; without even the implied warranty of                   
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    
-   MIT License for more details.                                                    
-                                                                                    
+   MIT License for more details.                                                                                                                                   
    You should have received a copy of the MIT License                               
    along with this program.  If not, see                                            
-   https://github.com/DevX-77/pufferpanel-installer/blob/main/LICENSE.              
-                                                                                    
+   https://github.com/DevX-77/pufferpanel-installer/blob/main/LICENSE.                                                                                            
    This script is not associated with the official Pufferpanel Project.             
-#######################################################################"
+ ######################################################################"
 echo "What would you like to do?:"
 echo "[1] Install PufferPanel On (x86 linux vps)"
 echo "[2] Install PufferPanel On (free/non pre-configured vps)"
