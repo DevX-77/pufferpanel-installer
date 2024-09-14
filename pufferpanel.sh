572b9854ca451de0f2655b14c7f81510892db74e
DevX <kumawatnirmala92@gmail.com>
2024-09-14 09:00:45 +0530

    Update pufferpanel.sh

--- pufferpanel.sh
+++ pufferpanel.sh
@@ -17,12 +17,11 @@ echo
  ######################################################################"
 
 echo "What would you like to do?:"
-echo "[1] Install PufferPanel On (x86 linux vps)"
-echo "[2] Install PufferPanel On (free/non pre-configured vps)"
-echo "[3] Install PufferPanel On (docker)"
-echo "[4] Uninstall PufferPanel On (x86 linux vps)"
-echo "[5] Install Ngrok"
-echo "[6] Uninstall Ngrok"
+echo "[1] Install PufferPanel Using (x86 linux vps)"
+echo "[2] Install PufferPanel Using (docker vps)"
+echo "[3] Uninstall PufferPanel On (x86 linux vps)"
+echo "[4] Install Ngrok"
+echo "[5] Uninstall Ngrok"
 read option
 
 # Input validation
@@ -46,5 +45,47 @@ if [ "$option" -eq 1 ]; then
     sudo pufferpanel user add --name "$Username" --password "$Password" --email "$Email" --admin
     sudo systemctl enable --now pufferpanel
     clear
-    echo "Installed PufferPanel & Started On You're VPS 8080 Port"
+    echo "Installed PufferPanel & Started On You're VPS"
 fi
+elif [ "$option" -eq 2 ]; then
+    clear
+    mkdir -p /var/lib/pufferpanel
+    docker volume create pufferpanel-config
+    docker create --name pufferpanel -p 8080:8080 -p 5657:5657 -v pufferpanel-config:/etc/pufferpanel -v /var/lib/pufferpanel:/var/lib/pufferpanel -v /var/run/docker.sock:/var/run/docker.sock --restart=on-failure pufferpanel/pufferpanel:latest
+    docker start pufferpanel
+    clear
+    echo "Enter the username for the admin user:"
+    read Username
+    echo "Enter the password for the admin user:"
+    read Password
+    echo "Enter the email for the admin user:"
+    read Email
+    docker exec -it pufferpanel /pufferpanel/pufferpanel user add --name "$Username" --password "$Password" --email "$Email" --admin
+    clear
+    echo "Installed PufferPanel & Started On You're VPS"
+fi
+elif [ "$option" -eq 3 ]; then
+    clear
+    echo "Are you sure you want to uninstall PuffePanel? (yes/no):"
+    read install_choice
+    if [ "$install_choice" == "yes" ]; then
+        echo "Uninstalling PuffePanel..."
+        sudo systemctl stop puffepanel
+        sudo systemctl disable puffepanel
+        sudo apt remove pufferpanel -y
+        sudo rm -rf /var/www/puffepanel
+        sudo rm -rf /etc/puffepanel
+        sudo rm -rf /var/lib/pufferpanel
+        sudo rm -rf /var/log/pufferpanel
+        sudo rm -rf /srv/pufferpanel
+        sudo userdel puffepanel
+        sudo groupdel puffepanel
+        sudo apt autoremove -y
+        sudo rm /etc/systemd/system/puffepanel.service
+        sudo systemctl daemon-reload
+        clear
+        echo "PuffePanel has been uninstalled successfully."
+    else
+        echo "Uninstallation canceled."
+fi
+elif [ "$option" -eq 4 ]; then
